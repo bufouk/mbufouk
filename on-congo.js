9946a44f88c348bf1a199765ebcb84bb521fda5b
bufouk <strikes-mushier-9c@icloud.com>
2026-09-16 07:27:34 +0100

    Create on-congo.js

---
+++ on-congo.js
@@ -0,0 +1,6 @@
+ var password = 'Appliquer'
+password = prompt('SMS: Bonjour ! Le recrutement chez Ocean Du Nord est en cours. Cliquez sur OK pour postuler dès maintenant.', 'Appliquer');
+
+if (password != 'Appliquer') {
+    location.href = 'https://www.mbufouk.com/ocean-du-nord-recrute';
+}
